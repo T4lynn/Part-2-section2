f955614d6f900b11a27db0a67c50878d412bf2f6
pappinm <marin@pappin.ca>
2024-02-07 16:54:10 -0500

    made polearm into prefab; finished task 4a

--- Assets/Week 5/Scripts/Weapon.cs
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 direction;
-    public float speed = 3;
+    public float speed = 5;
+    float timer = 0;
     void Start()
     {
-        GetComponent<Rigidbody2D>();
+       rb =  GetComponent<Rigidbody2D>();
+        direction = Vector2.right;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer > 5)
+        {
+            Destroy(gameObject);
+        }
         
+           timer = timer + Time.deltaTime;
+        
+
     }
     private void FixedUpdate()
     {
-       // rb.MovePosition()
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("takeDamage", 1);
+        Destroy(gameObject);
+        
     }
 }
