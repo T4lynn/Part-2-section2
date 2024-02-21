735dfcee066054132ca42cb87e5f4010c98a54d3
pappinm <marin@pappin.ca>
2024-02-21 17:33:13 -0500

    working on task 4

--- Assets/Week 7/scripts/GoalkeeperController.cs
+++ Assets/Week 7/scripts/GoalkeeperController.cs
@@ -10,11 +10,21 @@ public class GoalkeeperController : MonoBehaviour
    public Rigidbody2D rbcontroller;
     public Rigidbody2D rbselected;
     Vector2 currentvector;
+    Vector2 goalievector;
+    Vector2 direction;
 
     private void Start()
     {
-        FoodballPlayer.currentselection.transform = currentvector;
         rb = GetComponent<Rigidbody2D>();
+        goalievector = rb.transform.position;
+    }
+    private void Update()
+    {
+        FootballPlayerController.currentselection.transform.position = currentvector;
+    }
+    void finddirection()
+    {
+        direction = (goalievector - currentvector).normalized;
     }
 
 }
